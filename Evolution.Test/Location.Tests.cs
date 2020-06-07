4ccdadaad83a4e2664876cdc519a4307ad588454
George Botros <GBotros@ymail.com>
2020-06-07 16:08:02 +0100

    remove old test

--- Evolution.Test/Location.Tests.cs
+++ Evolution.Test/Location.Tests.cs
@@ -5,21 +5,8 @@ namespace Evolution.Test
     public class LocationTests
     {
         [Fact]
-        public void Move_OneAnimal_KeepOneAnimal()
+        public void Test1()
         {
-            //// arrange
-            //var nameHelper = new LocationNameHelper();
-            //var worldConfig = new WorldConfig {Size = 5};
-            //var logger = new Mock<ILogger<Map>>();
-            //var w = new Map(worldConfig, nameHelper, logger.Object);
-            //var dogy = w.Introduce("dogy");
-
-            //// act
-            //dogy.Live();
-
-            //// assert
-            //var spotsWithAnimals = w.Locations.Values.Count(l => l.HasAnimals());
-            //Assert.Equal(1, spotsWithAnimals);
         }
     }
 }
