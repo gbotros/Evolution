9090b4c67235c3f0ef02ebb0257384e1bd83bcf4
George Botros <GBotros@ymail.com>
2020-06-29 22:53:53 +0100

    use System.Net.Http.Json

--- Evolution.Services.Http/AnimalService.cs
+++ Evolution.Services.Http/AnimalService.cs
@@ -1,29 +1,33 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using System.Web;
 using Evolution.Abstractions;
 using Evolution.Entities;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace Evolution.Services.Http
 {
     public class AnimalService : IAnimalService
     {
-        public AnimalService(HttpClient httpClient)
+        public AnimalService(HttpClient httpClient, ILogger<IAnimalService> logger)
         {
             HttpClient = httpClient;
+            Logger = logger;
         }
 
         public HttpClient HttpClient { get; }
+        public ILogger Logger { get; }
 
         public async Task<bool> Add(AnimalBlueprint animal)
         {
-            var animalJson = JsonConvert.SerializeObject(animal);
-            using var animalContent = new StringContent(animalJson);
-            using var response = await HttpClient.PostAsync("Animals", animalContent);
+            if (animal == null) throw new ArgumentNullException(nameof(animal));
 
+            using var response = await HttpClient.PostAsJsonAsync("Animals", animal);
             return response.IsSuccessStatusCode;
         }
 
@@ -37,18 +41,15 @@ namespace Evolution.Services.Http
             query["LocationY"] = location.Y.ToString(CultureInfo.InvariantCulture);
             var queryFilter = query.ToString();
 
-            using var response = await HttpClient.GetAsync($"Animals?{queryFilter}");
-
-            var content = await response.Content.ReadAsStringAsync();
-            var creatures = JsonConvert.DeserializeObject<IEnumerable<AnimalBlueprint>>(content);
-            return creatures;
+            var animals = await HttpClient.GetFromJsonAsync<IEnumerable<AnimalBlueprint>>($"Animals?{queryFilter}");
+            return animals;
         }
 
         public async Task<bool> Update(AnimalBlueprint animal)
         {
-            var animalJson = JsonConvert.SerializeObject(animal);
-            using var animalContent = new StringContent(animalJson);
-            using var response = await HttpClient.PutAsync("Animals", animalContent);
+            if (animal == null) throw new ArgumentNullException(nameof(animal));
+
+            using var response = await HttpClient.PutAsJsonAsync($"Animals/{animal.Id}", animal);
 
             return response.IsSuccessStatusCode;
         }
