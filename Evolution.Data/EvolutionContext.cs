72fdfecea54557a7ebbaed0bd7fe560121353dca
George Habib <george.b.habib@gmail.com>
2021-03-21 18:09:27 -0700

    add data project

---
+++ Evolution.Data/EvolutionContext.cs
@@ -0,0 +1,21 @@
+﻿using Evolution.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evolution.Data
+{
+    public class EvolutionContext : DbContext
+    {
+
+        public DbSet<Animal> Animals { get; set; }
+        public DbSet<Plant> Plants { get; set; }
+
+
+    }
+
+
+}
---
+++ Evolution.Test.ConsoleClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Evolution.Test.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
