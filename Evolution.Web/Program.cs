4772b5b317010f59bc93b4daeb5a9f16306d6b73
George Habib <george.b.habib@gmail.com>
2021-05-31 15:21:43 -0700

    move blazor app to the root

--- Evolution.Web/Program.cs
+++ Evolution.Web/Program.cs
@@ -1,14 +1,10 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace Evolution.Web.Client
+namespace Evolution.Web
 {
     public class Program
     {
